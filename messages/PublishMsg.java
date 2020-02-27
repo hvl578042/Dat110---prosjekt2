836f147b10c7a0165b004d1471267f495494ae30
hvl578042 <malinive@hotmail.com>
2020-02-27 14:40:23 +0100

    first commit

---
+++ messages/PublishMsg.java
@@ -0,0 +1,13 @@
+package no.hvl.dat110.messages;
+
+public class PublishMsg extends Message {
+	
+	// message sent from client to create publish a message on a topic 
+
+	// TODO:
+	// Implement object variables - a topic and a message is required
+
+	// Constructor, get/set-methods, and toString method
+	// as described in the project text
+	
+}
